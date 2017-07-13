59680c83d2910f97930985b3b021bad57d7dc652
Vitaly Lobchuk <vitaly@lobchuk.ru>
2017-07-13 18:34:59 +0300

    fix(map): context

--- map.go
+++ map.go
@@ -1,27 +1,27 @@
 package jsonrpc
 
 import (
-	"context"
-	"fmt"
-	"reflect"
-	"strings"
-	"sync"
-	"unicode"
-	"unicode/utf8"
+    "context"
+    "fmt"
+    "reflect"
+    "strings"
+    "sync"
+    "unicode"
+    "unicode/utf8"
 
-	"github.com/pkg/errors"
+    "github.com/pkg/errors"
 )
 
 var (
-	// Precompute the reflect.Type of error and http.Request
-	typeOfError   = reflect.TypeOf((*error)(nil)).Elem()
-	typeOfContext = reflect.TypeOf((*context.Context)(nil)).Elem()
+    // Precompute the reflect.Type of error and http.Request
+    typeOfError   = reflect.TypeOf((*error)(nil)).Elem()
+    typeOfContext = reflect.TypeOf((*context.Context)(nil)).Elem()
 )
 
 var (
-	ErrRequestIllFormed = errors.New("service/method request ill-formed")
-	ErrServiceNotFound  = errors.New("can't find service")
-	ErrMethodNotFound   = errors.New("rpc: can't find method")
+    ErrRequestIllFormed = errors.New("service/method request ill-formed")
+    ErrServiceNotFound  = errors.New("can't find service")
+    ErrMethodNotFound   = errors.New("rpc: can't find method")
 )
 
 // ----------------------------------------------------------------------------
@@ -29,16 +29,16 @@ var (
 // ----------------------------------------------------------------------------
 
 type service struct {
-	name     string                    // name of service
-	rcvr     reflect.Value             // receiver of methods for the service
-	rcvrType reflect.Type              // type of the receiver
-	methods  map[string]*serviceMethod // registered methods
+    name     string                    // name of service
+    rcvr     reflect.Value             // receiver of methods for the service
+    rcvrType reflect.Type              // type of the receiver
+    methods  map[string]*serviceMethod // registered methods
 }
 
 type serviceMethod struct {
-	method    reflect.Method // receiver method
-	argsType  []reflect.Type // type of the request argument
-	replyType reflect.Type   // type of the response argument
+    method    reflect.Method // receiver method
+    argsType  []reflect.Type // type of the request argument
+    replyType reflect.Type   // type of the response argument
 }
 
 // ----------------------------------------------------------------------------
@@ -47,125 +47,122 @@ type serviceMethod struct {
 
 // serviceMap is a registry for services.
 type serviceMap struct {
-	mutex    sync.Mutex
-	services map[string]*service
+    mutex    sync.Mutex
+    services map[string]*service
 }
 
 // register adds a new service using reflection to extract its methods.
 func (m *serviceMap) register(rcvr interface{}, name string) error {
-	s := &service{
-		name:     name,
-		rcvr:     reflect.ValueOf(rcvr),
-		rcvrType: reflect.TypeOf(rcvr),
-		methods:  make(map[string]*serviceMethod),
-	}
+    s := &service{
+        name:     name,
+        rcvr:     reflect.ValueOf(rcvr),
+        rcvrType: reflect.TypeOf(rcvr),
+        methods:  make(map[string]*serviceMethod),
+    }
+    if name == "" {
+        s.name = reflect.Indirect(s.rcvr).Type().Name()
+        if !isExported(s.name) {
+            return fmt.Errorf("rpc: type %q is not exported", s.name)
+        }
+    }
+    if s.name == "" {
+        return fmt.Errorf("rpc: no service name for type %q", s.rcvrType.String())
+    }
+    for i := 0; i < s.rcvrType.NumMethod(); i++ {
+        method := s.rcvrType.Method(i)
+        mtype := method.Type
 
-	if name == "" {
-		s.name = reflect.Indirect(s.rcvr).Type().Name()
-		if !isExported(s.name) {
-			return fmt.Errorf("rpc: type %q is not exported", s.name)
-		}
-	}
+        // Method must be exported.
+        if method.PkgPath != "" {
+            continue
+        }
 
-	if s.name == "" {
-		return fmt.Errorf("rpc: no service name for type %q", s.rcvrType.String())
-	}
+        var args []reflect.Type
 
-	for i := 0; i < s.rcvrType.NumMethod(); i++ {
-		method := s.rcvrType.Method(i)
-		mtype := method.Type
+        numIn := mtype.NumIn()
 
-		// Method must be exported.
-		if method.PkgPath != "" {
-			continue
-		}
+        for i := 1; i < numIn; i++ {
+            arg := mtype.In(i)
+            if arg.Kind() != reflect.Interface && (arg.Kind() != reflect.Ptr || !isExportedOrBuiltin(arg)) {
+                continue
+            }
+            if arg.Kind() != typeOfContext.Kind() {
+                arg = arg.Elem()
+            }
+            args = append(args, arg)
+        }
+        if numIn-1 != len(args) {
+            continue
+        }
+        // Method needs two out: mixed, error.
+        if mtype.NumOut() != 2 {
+            continue
+        }
+        if returnType := mtype.Out(1); returnType != typeOfError {
+            continue
+        }
+        s.methods[method.Name] = &serviceMethod{
+            method:   method,
+            argsType: args,
+        }
+    }
 
-		var args []reflect.Type
+    if len(s.methods) == 0 {
+        return fmt.Errorf("rpc: %q has no exported methods of suitable type", s.name)
+    }
 
-		numIn := mtype.NumIn()
+    m.mutex.Lock()
 
-		for i := 1; i < numIn; i++ {
-			arg := mtype.In(i)
-			if arg.Kind() != reflect.Interface {
-				if arg.Kind() != reflect.Ptr || !isExportedOrBuiltin(arg) {
-					continue
-				}
-				arg = arg.Elem()
-			}
-			args = append(args, arg)
-		}
-		if numIn-1 != len(args) {
-			continue
-		}
-		// Method needs two out: mixed, error.
-		if mtype.NumOut() != 2 {
-			continue
-		}
-		if returnType := mtype.Out(1); returnType != typeOfError {
-			continue
-		}
-		s.methods[method.Name] = &serviceMethod{
-			method:   method,
-			argsType: args,
-		}
-	}
+    defer m.mutex.Unlock()
 
-	if len(s.methods) == 0 {
-		return fmt.Errorf("rpc: %q has no exported methods of suitable type", s.name)
-	}
+    if m.services == nil {
+        m.services = make(map[string]*service)
+    } else if _, ok := m.services[s.name]; ok {
+        return fmt.Errorf("rpc: service already defined: %q", s.name)
+    }
 
-	m.mutex.Lock()
+    m.services[s.name] = s
 
-	defer m.mutex.Unlock()
-
-	if m.services == nil {
-		m.services = make(map[string]*service)
-	} else if _, ok := m.services[s.name]; ok {
-		return fmt.Errorf("rpc: service already defined: %q", s.name)
-	}
-
-	m.services[s.name] = s
-
-	return nil
+    return nil
 }
 
 // get returns a registered service given a method name.
 //
 // The method name uses a dotted notation as in "Service.Method".
 func (m *serviceMap) get(method string) (*service, *serviceMethod, error) {
-	parts := strings.Split(method, ".")
-	if len(parts) != 2 {
-		return nil, nil, ErrRequestIllFormed
-	}
+    parts := strings.Split(method, ".")
+    if len(parts) != 2 {
+        return nil, nil, ErrRequestIllFormed
+    }
 
-	m.mutex.Lock()
-	service := m.services[parts[0]]
-	m.mutex.Unlock()
+    m.mutex.Lock()
+    service := m.services[parts[0]]
+    m.mutex.Unlock()
 
-	if service == nil {
-		return nil, nil, ErrServiceNotFound
-	}
+    if service == nil {
+        return nil, nil, ErrServiceNotFound
+    }
 
-	serviceMethod := service.methods[parts[1]]
-	if serviceMethod == nil {
-		return nil, nil, ErrMethodNotFound
-	}
+    serviceMethod := service.methods[parts[1]]
+    if serviceMethod == nil {
+        return nil, nil, ErrMethodNotFound
+    }
 
-	return service, serviceMethod, nil
+    return service, serviceMethod, nil
 }
 
 // isExported returns true of a string is an exported (upper case) name.
 func isExported(name string) bool {
-	rune, _ := utf8.DecodeRuneInString(name)
-	return unicode.IsUpper(rune)
+    rune, _ := utf8.DecodeRuneInString(name)
+    return unicode.IsUpper(rune)
 }
 
 // isExportedOrBuiltin returns true if a type is exported or a builtin.
 func isExportedOrBuiltin(t reflect.Type) bool {
-	for t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-	// PkgPath will be non-empty even for an exported type,
-	// so we need to check the type name as well.
-	return isExported(t.Name()) || t.PkgPath() == ""
+    for t.Kind() == reflect.Ptr {
+        t = t.Elem()
+    }
+    // PkgPath will be non-empty even for an exported type,
+    // so we need to check the type name as well.
+    return isExported(t.Name()) || t.PkgPath() == ""
 }
--- server.go
+++ server.go
@@ -1,11 +1,11 @@
 package jsonrpc
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"reflect"
-	"strings"
+    "context"
+    "fmt"
+    "net/http"
+    "reflect"
+    "strings"
 )
 
 // ----------------------------------------------------------------------------
@@ -14,22 +14,22 @@ import (
 
 // Codec creates a CodecRequest to process each request.
 type Codec interface {
-	NewRequest(*http.Request) CodecRequest
+    NewRequest(*http.Request) CodecRequest
 }
 
 // CodecRequest decodes a request and encodes a response using a specific
 // serialization scheme.
 type CodecRequest interface {
-	// Reads the request and returns the RPC method name.
-	Method() (string, error)
-	// Reads the request filling the RPC method args.
-	ReadRequest(interface{}) error
-	// Writes the response using the RPC method reply.
-	WriteResponse(http.ResponseWriter, interface{})
-	// Writes an error produced by the server.
-	WriteError(w http.ResponseWriter, status int, err error)
-	// Get raw body
-	Body() []byte
+    // Reads the request and returns the RPC method name.
+    Method() (string, error)
+    // Reads the request filling the RPC method args.
+    ReadRequest(interface{}) error
+    // Writes the response using the RPC method reply.
+    WriteResponse(http.ResponseWriter, interface{})
+    // Writes an error produced by the server.
+    WriteError(w http.ResponseWriter, status int, err error)
+    // Get raw body
+    Body() []byte
 }
 
 // ----------------------------------------------------------------------------
@@ -40,27 +40,27 @@ type ServerBeforeFunc func(ctx context.Context, method string, header http.Heade
 
 // Server serves registered RPC services using registered codecs.
 type Server struct {
-	codecs   map[string]Codec
-	services *serviceMap
-	before   []ServerBeforeFunc
+    codecs   map[string]Codec
+    services *serviceMap
+    before   []ServerBeforeFunc
 }
 
 type ServerOption func(*Server)
 
 func ServerBefore(before ServerBeforeFunc) ServerOption {
-	return func(s *Server) { s.before = append(s.before, before) }
+    return func(s *Server) { s.before = append(s.before, before) }
 }
 
 // NewServer returns a new RPC server.
 func NewServer(options ...ServerOption) *Server {
-	s := &Server{
-		codecs:   make(map[string]Codec),
-		services: new(serviceMap),
-	}
-	for _, option := range options {
-		option(s)
-	}
-	return s
+    s := &Server{
+        codecs:   make(map[string]Codec),
+        services: new(serviceMap),
+    }
+    for _, option := range options {
+        option(s)
+    }
+    return s
 }
 
 // RegisterCodec adds a new codec to the server.
@@ -69,7 +69,7 @@ func NewServer(options ...ServerOption) *Server {
 // XML. A codec is chosen based on the "Content-Type" header from the request,
 // excluding the charset definition.
 func (s *Server) RegisterCodec(codec Codec, contentType string) {
-	s.codecs[strings.ToLower(contentType)] = codec
+    s.codecs[strings.ToLower(contentType)] = codec
 }
 
 // RegisterService adds a new service to the server.
@@ -89,110 +89,108 @@ func (s *Server) RegisterCodec(codec Codec, contentType string) {
 //
 // All other methods are ignored.
 func (s *Server) RegisterService(receiver interface{}, name string) error {
-	return s.services.register(receiver, name)
+    return s.services.register(receiver, name)
 }
 
 // HasMethod returns true if the given method is registered.
 //
 // The method uses a dotted notation as in "Service.Method".
 func (s *Server) HasMethod(method string) bool {
-	if _, _, err := s.services.get(method); err == nil {
-		return true
-	}
-	return false
+    if _, _, err := s.services.get(method); err == nil {
+        return true
+    }
+    return false
 }
 
 // ServeHTTP
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+    ctx := r.Context()
 
-	if r.Method != "POST" {
-		WriteError(w, 405, "rpc: POST method required, received "+r.Method)
-		return
-	}
-	contentType := r.Header.Get("Content-Type")
-	idx := strings.Index(contentType, ";")
+    if r.Method != "POST" {
+        WriteError(w, 405, "rpc: POST method required, received "+r.Method)
+        return
+    }
+    contentType := r.Header.Get("Content-Type")
+    idx := strings.Index(contentType, ";")
 
-	if idx != -1 {
-		contentType = contentType[:idx]
-	}
+    if idx != -1 {
+        contentType = contentType[:idx]
+    }
 
-	var codec Codec
+    var codec Codec
 
-	if contentType == "" && len(s.codecs) == 1 {
-		// If Content-Type is not set and only one codec has been registered,
-		// then default to that codec.
-		for _, c := range s.codecs {
-			codec = c
-		}
-	} else if codec = s.codecs[strings.ToLower(contentType)]; codec == nil {
-		WriteError(w, 415, "rpc: unrecognized Content-Type: "+contentType)
-		return
-	}
+    if contentType == "" && len(s.codecs) == 1 {
+        // If Content-Type is not set and only one codec has been registered,
+        // then default to that codec.
+        for _, c := range s.codecs {
+            codec = c
+        }
+    } else if codec = s.codecs[strings.ToLower(contentType)]; codec == nil {
+        WriteError(w, 415, "rpc: unrecognized Content-Type: "+contentType)
+        return
+    }
 
-	// Create a new codec request.
-	codecReq := codec.NewRequest(r)
+    // Create a new codec request.
+    codecReq := codec.NewRequest(r)
 
-	// Get service method to be called.
-	method, errMethod := codecReq.Method()
-	if errMethod != nil {
-		codecReq.WriteError(w, 400, errMethod)
-		return
-	}
+    // Get service method to be called.
+    method, errMethod := codecReq.Method()
+    if errMethod != nil {
+        codecReq.WriteError(w, 400, errMethod)
+        return
+    }
 
     for _, before := range s.before {
         ctx = before(ctx, method, r.Header, codecReq)
     }
 
-	serviceSpec, methodSpec, errGet := s.services.get(method)
-	if errGet != nil {
-		codecReq.WriteError(w, 400, errGet)
-		return
-	}
-	refValue := []reflect.Value{serviceSpec.rcvr}
-	// Decode the args.
-	if len(methodSpec.argsType) > 0 {
-		for i := 0; i < len(methodSpec.argsType); i++ {
-			arg := reflect.New(methodSpec.argsType[i])
+    serviceSpec, methodSpec, errGet := s.services.get(method)
+    if errGet != nil {
+        codecReq.WriteError(w, 400, errGet)
+        return
+    }
+    refValue := []reflect.Value{serviceSpec.rcvr}
+    // Decode the args.
+    if len(methodSpec.argsType) > 0 {
+        for i := 0; i < len(methodSpec.argsType); i++ {
+            arg := reflect.New(methodSpec.argsType[i])
+            if methodSpec.argsType[i] != typeOfContext {
+                if errRead := codecReq.ReadRequest(arg.Interface()); errRead != nil {
+                    codecReq.WriteError(w, 400, errRead)
+                    return
+                }
+            } else {
+                arg = reflect.ValueOf(ctx)
+            }
+            refValue = append(refValue, arg)
+        }
+    }
 
-			if methodSpec.argsType[i] != typeOfContext {
-				if errRead := codecReq.ReadRequest(arg.Interface()); errRead != nil {
-					codecReq.WriteError(w, 400, errRead)
-					return
-				}
-			} else {
-				arg = reflect.ValueOf(ctx)
-			}
+    retValues := methodSpec.method.Func.Call(refValue)
 
-			refValue = append(refValue, arg)
-		}
-	}
+    // Cast the result to error if needed.
+    var errResult error
+    errInter := retValues[1].Interface()
+    if errInter != nil {
+        errResult = errInter.(error)
+    }
 
-	retValues := methodSpec.method.Func.Call(refValue)
+    // Prevents Internet Explorer from MIME-sniffing a response away
+    // from the declared content-type
+    w.Header().Set("x-content-type-options", "nosniff")
 
-	// Cast the result to error if needed.
-	var errResult error
-	errInter := retValues[1].Interface()
-	if errInter != nil {
-		errResult = errInter.(error)
-	}
-
-	// Prevents Internet Explorer from MIME-sniffing a response away
-	// from the declared content-type
-	w.Header().Set("x-content-type-options", "nosniff")
-
-	// Encode the response.
-	if errResult == nil {
-		valRet := retValues[0].Interface()
-		codecReq.WriteResponse(w, valRet)
-	} else {
-		codecReq.WriteError(w, 400, errResult)
-	}
+    // Encode the response.
+    if errResult == nil {
+        valRet := retValues[0].Interface()
+        codecReq.WriteResponse(w, valRet)
+    } else {
+        codecReq.WriteError(w, 400, errResult)
+    }
 }
 
 // WriteError send error to client
 func WriteError(w http.ResponseWriter, status int, msg string) {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprint(w, msg)
+    w.WriteHeader(status)
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    fmt.Fprint(w, msg)
 }
